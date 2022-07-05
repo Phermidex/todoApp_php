a40ed7626ba8ee487f48ffe3ab908edb5497a3b1
Hermides Miguel Medina Morales <hermides07@gmail.com>
2022-07-05 18:04:29 -0400

    Update Tools.php

--- core/Tools.php
+++ core/Tools.php
@@ -8,7 +8,7 @@ class Tools {
         $url = '';
     
         if(isset( $_SERVER["HTTP"] ) && strtolower( $_SERVER["HTTP"] ) == "on"){
-            $url .= 'https://'.$_SERVER["HTTP_HOST"].$_SERVER["REQUEST_URI"].$uri;
+            $url .= 'https://todoapp345.herokuapp.com/index.php/';
         }else{
             $url .= 'http://'.$_SERVER["HTTP_HOST"].$_SERVER["REQUEST_URI"].$uri;
         }
