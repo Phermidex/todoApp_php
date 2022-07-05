6e9e0da8a1164da4e228cf4ade3682c29ef7d52a
Hermides Miguel Medina Morales <hermides07@gmail.com>
2022-07-05 17:39:47 -0400

    Update database.php

--- config/database.php
+++ config/database.php
@@ -3,10 +3,11 @@
 
 return [
         "database" => [
-                'dbname' => 'todoapp',
-                'username' => 'root',
-                'password' => '',
-                'host' => '127.0.0.1',
+                'dbname' => 'sql3504274',
+                'username' => 'sql3504274',
+                'password' => 'g2bG7JfEZv',
+                'host' => 'sql3.freesqldatabase.com',
+                'port' => '3306',
                 'options' => [
 
                 ]
